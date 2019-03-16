d5e0cf3ebda07aa3b007dbbec043b212d238592f
github@cybertekschool.com <github@cybertekschool.com>
2019-03-16 16:50:52 -0400

    added dynamic team name creation

--- src/test/java/com/app/tests/PostRequestExample.java
+++ src/test/java/com/app/tests/PostRequestExample.java
@@ -1,11 +1,17 @@
 package com.app.tests;
 
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 
 public class PostRequestExample {
 
@@ -21,6 +27,28 @@ public class PostRequestExample {
 
         String token = BookITRestUtility.getTeacherToken();
 
+        String team =new Faker().team().name();
+        String batch = "8";
+        RestAssured.given().
+                header("Authorization", token).
+                queryParam("campus-location", "VA").
+                queryParam("batch-number", batch).
+                queryParam("team-name", team).
+                when().post("/api/teams/team").
+                then().log().all().
+                statusCode(201).
+                body(is("team "+team+" has been added to the batch "+batch+"."));
+
+        verifyTeamExists(team);
     }
+
+    public static void verifyTeamExists(String team) {
+        DatabaseUtility.createConnection();
+        String sql = "select * from team where name = '" + team + "';";
+        System.out.println(sql);
+        Map<String, Object> result = DatabaseUtility.getRowMap(sql);
+        Assert.assertNotNull(result);
+    }
+
 
 }
